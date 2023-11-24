6e2db9daa75bf480a140b12a96a817f6fdd7e1fa
FlavioSantos2002 <luizflavio.20052002@gmail.com>
2023-11-24 15:34:36 -0400

    novo

--- js/script.js
+++
@@ -1,6 +0,0 @@
- function toggleMenu() {
-        var menu = document.querySelectorAll('.bot');
-        for (var i = 0; i < menu.length; i++) {
-            menu[i].style.display = (menu[i].style.display === 'none' || menu[i].style.display === '') ? 'block' : 'none';
-        }
-}
